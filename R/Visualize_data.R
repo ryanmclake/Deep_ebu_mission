3df8b69aed56980f2a55d58422dcb13758f193a4
Ryan McClure <rmcclure@CIS-RMcclure-Air.local>
2022-10-30 12:36:38 -0400

    add welches t test

--- R/Visualize_data.R
+++ R/Visualize_data.R
@@ -1,6 +1,18 @@
 
-
+#Visual time series of 17 and 18
 ebu50 <- dt1 %>% filter(Transect =="T5") %>%
   filter(Site %in% c("T5e2","T5e3")) %>%
   ggplot(.,)+
   geom_point(aes(x = DateTime, y = Ebu_rate, color = Site))
+
+ebu50
+
+ebu_17 <- dt1 %>% 
+  mutate(year = lubridate::year(DateTime)) %>% filter(year == "2017")
+ebu_18 <- dt1 %>% 
+  mutate(year = lubridate::year(DateTime)) %>% filter(year == "2018")
+
+
+# Run a Welches t.test --> KISS
+t.test(ebu_17$Ebu_rate, ebu_18$Ebu_rate, var.equal=FALSE)
+
